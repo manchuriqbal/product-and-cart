175723bf439a262e69be128d9d1c0e53008c287b
manchur <manchur.fiverr@gmail.com>
2023-08-14 09:22:03 +0600

    I complete vue js dynamic product and cart project

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,26 @@
+<template>
+ <main class="wrapper">
+   <h1>Products</h1>
+
+   <div class="card-container">
+    <ProductCart
+      v-for="(product, index) in inventory.slice(0,6)"
+      :key="product.id"
+      class="card"
+      :index="index"
+      :product="product"
+      :addToCart="addToCart"
+    />
+   </div>
+  </main>
+</template>
+
+<script>
+import ProductCart from '@/components/ProductCart.vue'
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCart
+  }
+}
+</script>
